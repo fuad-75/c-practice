61678e440f2256aabe628f75b085424185c6a3ea
fuad-75 <fuadsalimfs2007@gmail.com>
2026-06-27 23:36:59 +0600

    Create reverseThreeDigitNum.c

---
+++ reverseThreeDigitNum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int p1,p2,p3;
+    printf("Enter a three digit number:");
+    scanf("%1d%1d%1d",&p1,&p2,&p3);
+    printf(" The reverse is :%d%d%d",p3,p2,p1);
+
+    return 0;
+}
