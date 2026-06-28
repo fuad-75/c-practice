89463efc834bada36cfde6b5f5d52d755e7b6331
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-06-29 00:30:16 +0600

    Update sumOFdigits.c

--- sumOFdigits.c
+++ sumOFdigits.c
@@ -32,3 +32,16 @@ int main()
 
     return 0;
 }
+// Alternate and Fast Solution:
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    int sum=0;
+    while(n>0){
+        sum=sum+n%10;
+        n=n/10;
+    }
+    printf("The sum is %d",sum);
+}
