61996f0a24caea259acc2907242d5ee87e8597b0
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-06-28 16:20:13 +0600

    Create sumOFdigits.c

---
+++ sumOFdigits.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int n,m,k,k1,f1,sum;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    m=0;
+    for(int i=10;;i*=10)
+    {
+        k=n/i;
+        m=m+1;
+        if(k==0){
+            
+            break;
+        }
+    }
+    sum=0;
+    for(int i=pow(10,m-1);;i=i/10){
+        k1=n/i;
+        f1=n%i;
+        n=f1;
+        sum=sum+k1;
+        if(i==1){
+            
+            printf("The sum is :%d",sum);
+            return 0;
+        }
+    }
+    
+
+    return 0;
+}
