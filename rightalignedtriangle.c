a00f15f2d4237ead4b90e919608d70bf162579b9
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-07-09 01:22:18 +0600

    Update rightalignedtriangle.c

--- rightalignedtriangle.c
+++ rightalignedtriangle.c
@@ -17,3 +17,25 @@ int main(){
         
     }
 }
+//Alternative
+#include<stdio.h>
+int main(){
+    int n;
+    int a;
+    char ch='*';
+    printf("Enter the order you want:");
+    scanf("%d",&n);
+    int j=1;
+    for(int i=1;i<=n;i++){
+            int a=65;
+        for(int j=1;j<=n;j++){
+            if((j==n)||(i==n)||((i+j)>n)){
+            printf("%c",a);
+            a=a+1;}
+            else
+            printf(" ");
+        }
+        printf("\n");
+        
+    }
+}
