9de114bca61d39190df5d93db6e2528ae10b64be
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-07-11 00:49:34 +0600

    Create numbergate.c

---
+++ numbergate.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("Enter order:");
+    scanf("%d",&n);
+    for(int g=1;g<=(2*n-1);g++){
+        printf("%d",g);
+    }
+    printf("\n");
+    m=n-1;
+    int nst=m;
+    int nsp=1;
+    int a;
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=nst;j++){
+            printf("%d",j);
+            a=2*n-j;
+        }
+        for(int k=1;k<=nsp;k++){
+            printf(" ");
+        }
+        for(a;a<=(2*n-1);a++){
+            printf("%d",a);
+        }
+        nsp+=2;
+        nst--;
+        printf("\n");
+        
+    }
+}
