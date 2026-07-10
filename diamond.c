13c00c98b072890d989c58fc82016c39ba47ec56
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-07-10 22:47:36 +0600

    Create diamond.c

---
+++ diamond.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n;
+    char c='*';
+    printf("Enter the order:");
+    scanf("%d",&n);
+    int k=1;
+    for(int i=1;i<=n;i++){
+       
+        for(k=1;k<=(n-i);k++){
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++){
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+    int m;
+    for(int i=n-1;i>=1;i--){
+        for(m=n-i;m>=1;m--){
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++){
+            printf("%c",c);
+        }
+        printf("\n");
+    }
+}
