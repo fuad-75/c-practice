c9da0e831a20ffe6b169a1787f65ca7acc9d3941
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-07-11 01:08:14 +0600

    Update numbergate.c

--- numbergate.c
+++ numbergate.c
@@ -62,3 +62,41 @@ int main(){
         
     }
 }
+// OR 1234567
+      123 321
+      12   21
+      1     1
+
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("Enter order:");
+    scanf("%d",&n);
+    for(int g=1;g<=(2*n-1);g++){
+        printf("%d",g);
+    }
+    printf("\n");
+    m=n-1;
+    int nst=m;
+    int nsp=1;
+    
+    for(int i=1;i<=m;i++){
+        int a=1;
+        for(int j=1;j<=nst;j++){
+            printf("%d",a);
+            a++;
+        }
+        for(int k=1;k<=nsp;k++){
+            printf(" ");
+            
+        }
+        for(int j=1;j<=nst;j++){
+            printf("%d",a-1);
+            a--;
+        }
+        nsp+=2;
+        nst--;
+        printf("\n");
+        
+    }
+}
