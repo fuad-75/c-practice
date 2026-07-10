c7361e968d3c3807ec5a32669f95fd0631e5d69f
Fuad Salim <fuadsalimfs2007@gmail.com>
2026-07-10 20:55:19 +0600

    Update pyramid.c

--- pyramid.c
+++ pyramid.c
@@ -23,3 +23,25 @@ int main() {
     
     return 0;
 }
+//OR 
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the order: ");
+    scanf("%d", &n);
+    
+    char ch = '*';
+    
+    for (int i = 1; i <= n; i++) {
+        for(int k=1;k<=(n-i);k++){
+            printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++){
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
